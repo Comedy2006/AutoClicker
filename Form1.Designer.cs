bb4d89dfbf19270458a1fe2fb57dcc2f36839249
Lorenz <Lorenz@DESKTOP-I9NSBPF>
2021-10-25 17:25:56 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace AutoClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.backgroundKeys = new System.ComponentModel.BackgroundWorker();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(31, 50);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(108, 49);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start (F7)";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(172, 50);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(108, 49);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Stop (F7)";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Click interval";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ms";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(119, 23);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // backgroundKeys
+            // 
+            this.backgroundKeys.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundKeys_DoWork);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(184, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "github.com/Comedy2006";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 126);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Speedy AutoClicker v1.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.ComponentModel.BackgroundWorker backgroundKeys;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// AutoClicker by github.com/Comedy2006
+
+namespace AutoClicker
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private const int LEFTUP_BTT = 0x0004;
+        private const int LEFTDOWN_BTT = 0x0002;
+
+        public int intervalms;
+        public bool isPressed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread AutoClicker = new Thread(clicker);
+            AutoClicker.Start();
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+            backgroundKeys.RunWorkerAsync();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            isPressed = true;
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+        }
+
+
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            isPressed = false;
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+        }
+
+        private void clicker()
+        {
+            while (true)
+            {
+                if (isPressed == true)
+                {
+                    mouse_event(dwFlags: LEFTUP_BTT, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
+                    Thread.Sleep(1);
+                    mouse_event(dwFlags: LEFTDOWN_BTT, dx: 0, dy: 0, cButtons: 0, dwExtraInfo: 0);
+                    Thread.Sleep(intervalms);
+                }
+                Thread.Sleep(3);
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            intervalms = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+        }
+
+        private void backgroundKeys_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                if (buttonStart.Enabled == true)
+                {
+                    if (GetAsyncKeyState(Keys.F7) < 0)
+                    {
+                        buttonStart.PerformClick();
+                        Thread.Sleep(250);
+                    }
+                }
+                else if (buttonStop.Enabled == true)
+                {
+                    if (GetAsyncKeyState(Keys.F7) < 0)
+                    {
+                        buttonStop.PerformClick();
+                        Thread.Sleep(250);
+                    }
+
+                }
+                Thread.Sleep(1);
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
